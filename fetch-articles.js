88245a5b3febda2fc4f8029896f413dc40f152ab
mikowals <mikowals@gmail.com>
2020-12-11 14:17:16 +1000

    format articles for db

--- fetch-articles.js
+++ fetch-articles.js
@@ -1,30 +1,45 @@
 import Parser from 'rss-parser';
 import { filter, flatMap } from 'lodash';
+import { Article } from './article';
 
-const timedFetch = async (url) => {
+let parser = new Parser({
+  customFields: {
+    item: [
+      ['creator', 'author'],
+      ['content', 'summary'],
+      ['pubDate', 'date'],
+    ]
+  }
+});
+
+const fetchFeeds = async (url) => {
   console.time(url);
   const httpResponse = await fetch(url);
+  const rssString = await httpResponse.text();
   console.timeEnd(url);
-  return httpResponse;
-};
-
-const parseFeed = async (pagePromise) => {
-  const pageResponse = await pagePromise;
-  const rssString = await pageResponse.text();
-  let feed = await new Parser().parseString(rssString);
-  return removeOldArticles(feed.items, 2);
+  let feed = await parser.parseString(rssString);
+  console.log(feed.items[0]);
+  feed.etag = httpResponse.headers.etag
+  feed.lastModified = httpResponse.headers["last-modified"]
+  return feed;
 };
 
 export const fetchArticles = async (feedURLs) => {
-  const pagePromises = feedURLs.map(timedFetch);
-  const articlesByFeed = await Promise.all(pagePromises.map(parseFeed));
-  return articlesByFeed.flat();
+  const feeds = await Promise.all(feedURLs.map(fetchFeeds));
+  //const feeds = await Promise.all(pagePromises.map(parseFeed));
+  return flatMap(feeds, prepareArticlesForDB);
 };
 
+const prepareArticlesForDB = (feed) => {
+  let articles = feed.items.map(article => {
+    return new Article(article, feed);
+  });
+  return removeOldArticles(articles, 2);
+}
 const removeOldArticles = (articles, daysOld) => {
   let twoDaysAgo = new Date();
   twoDaysAgo.setDate(twoDaysAgo.getDate() - daysOld);
   return filter(articles, (value) => {
-    return new Date(value.pubDate) >= twoDaysAgo;
+    return value.date >= twoDaysAgo;
   });
 };
--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-import { Collection } from './collection';
 import loki from 'lokijs';
 import { fetchArticles } from './fetch-articles';
-var db = {
-  articles: new Collection({uniqueFields: ["link"]}),
-  feeds: new Collection({uniqueFields: ["url"]})
-};
 
 var lokiDB = new loki('rss');
 let articles = lokiDB.addCollection("articles", {
@@ -18,6 +13,19 @@ addEventListener('fetch', event => {
 })
 
 async function handleRequest(request) {
+  if (! articles.findOne()) {
+    let savedArticles = await RSS.get('articles', 'json');
+    console.log("found ", savedArticles.length, " articles in Worker KV");
+    //db.articles.insert(savedArticles);
+    savedArticles.forEach(doc => {
+      try {
+        articles.insert(doc);
+      } catch(err) {
+        //console.log(err.message)
+      }
+    });
+  }
+
   const url = new URL(request.url);
   switch (url.pathname) {
   case "/updateDB":
@@ -30,39 +38,28 @@ async function handleRequest(request) {
     ];
     console.time("fetchArticles");
     const newArticles = await fetchArticles(feedURLs);
-    let itemsInserted = db.articles.insert(newArticles);
+    let itemsInserted = 0;
+    newArticles.forEach(article => {
+      try {
+        articles.insert(article);
+        itemsInserted += 1;
+      } catch(e) {}
+    });
+
     console.timeEnd("fetchArticles")
-    let kvInsert = await RSS.put(
+
+    // insert async to KV to respond to user
+    let kvInsert = RSS.put(
         "articles",
-        JSON.stringify(db.articles.find({}, {
-          fields: {
-            _id: 1,
-            link: 1,
-            title: 1,
-            pubDate: 1,
-            contentSnippet: 1,
-            guid: 1
-          }
-        })),
+        JSON.stringify(articles.find()),
         {expirationTtl: 2 * 24 * 60 * 60} // expire in 2 days
     );
 
-    return new Response("Inserted " +  itemsInserted + " new articles.", {
+    return new Response("Inserted " +  itemsInserted + " new articles in database.", {
       headers: { 'content-type': 'text/plain' },
     });
     break;
   default:
-    let savedArticles = await RSS.get('articles', 'json');
-    console.log("found ", savedArticles.length, " articles");
-    //db.articles.insert(savedArticles);
-    savedArticles.forEach(doc => {
-      try {
-        articles.insert(doc);
-      } catch(err) {
-        //console.log(err.message)
-      }
-    });
-    
     return new Response(
       JSON.stringify(articles
         .chain()
